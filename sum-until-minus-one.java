3a7c670157cbede4b91347361ab5cb12f13713c5
Polina Rylova <mail@polinarya.ru>
2018-01-24 21:22:14 +0500

    Суммировать до ввода "-1", затем вывести результат

---
+++ sum-until-minus-one.java
@@ -0,0 +1,31 @@
+/* 
+Суммирование
+
+Вводить с клавиатуры числа.
+Если пользователь ввел -1, вывести на экран сумму всех введенных чисел и завершить программу.
+-1 должно учитываться в сумме.
+
+Требования:
+1. Программа должна считывать числа c клавиатуры.
+2. Программа должна выводить число на экран.
+3. Программа должна посчитать сумму введенных чисел. Если пользователь ввел -1, вывести на экран сумму и завершить программу.
+4. В программе должен использоваться цикл for или цикл while.
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        int num = 0;
+
+        while (num != -1) {
+           num = Integer.parseInt(reader.readLine());
+           sum += num;
+        }
+        System.out.println(sum);
+
+    }
+}
